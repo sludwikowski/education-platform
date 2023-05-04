0747ead07ab04302398e2974c72c18a3efe98e2e
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-05-04 21:36:26 +0200

    fix: page fix

--- apps/next/app/page.tsx
+++ apps/next/app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <Button type="submit" intent="secondary" />
+      <Button intent="secondary" />
     </main>
   )
 }
