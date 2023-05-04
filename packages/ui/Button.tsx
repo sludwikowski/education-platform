911f610b11c8166e11e21888022663b54ce87f95
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-05-04 21:25:19 +0200

    style(button): add variants add props and buttonClasses to the `Button` component BREAKING CHANGES: add type `submit` to the Button component for fix eslint problems

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,8 +1,65 @@
-export function Button() {
+import { cva, VariantProps } from 'class-variance-authority'
+
+const buttonClasses = cva(
+  [
+    'rounded-3xl',
+    'font-bold',
+    'hover:scale-110',
+    'active:scale-100',
+    'transition',
+    'duration-200',
+    'ease-in-out',
+  ],
+  {
+    variants: {
+      intent: {
+        primary: [
+          'bg-blue-700',
+          'text-white',
+          'border-transparent',
+          'hover:bg-blue-800',
+        ],
+
+        secondary: [
+          'bg-white',
+          'text-black',
+          'border-gray-400',
+          'hover:bg-gray-100',
+          'border-solid',
+          'border-2',
+          'border-gray-800',
+        ],
+        text: ['bg-transparent', 'text-black', 'hover:bg-gray-100'],
+      },
+      size: {
+        small: ['text-md', 'py-1', 'px-2'],
+        medium: ['text-lg', 'px-6', 'py-2'],
+        large: ['text-xlg', 'px-8', 'py-4'],
+      },
+    },
+    defaultVariants: {
+      intent: 'primary',
+      size: 'medium',
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.HtmlHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonClasses> {}
+
+export function Button({
+  children,
+  className,
+  intent,
+  size,
+  ...props
+}: ButtonProps) {
   return (
     <button
-      type="button"
-      className="h-16 w-16 bg-blue-500 hover:bg-blue-700"
+      className={buttonClasses({ intent, size, className })}
+      {...props}
+      type="submit"
     >
       Boop
     </button>
